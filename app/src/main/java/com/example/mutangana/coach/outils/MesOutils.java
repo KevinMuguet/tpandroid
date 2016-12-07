f4f7d72385e46ab90bc117ccb201660bc0024c44
Mutangana <mut_kevin@yahoo.fr>
2016-12-07 09:14:25 +0100

    "séquence 6 page 41

---
+++ app/src/main/java/com/example/mutangana/coach/outils/MesOutils.java
@@ -0,0 +1,38 @@
+package com.example.mutangana.coach.outils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Mutangana on 06/12/2016.
+ */
+
+public abstract class MesOutils {
+
+    /**
+     *  le formatage de la date
+     * @param
+     * @return
+     */
+    public static Date convertStringToDate(String unedate) {
+
+         
+        String expectedPattern ="dd/MM/yy HH:mm:ss";
+        SimpleDateFormat formatter = new SimpleDateFormat(expectedPattern);
+        try {
+
+            Date date = formatter.parse(unedate);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+
+    }
+    public static String convertDateToString(Date uneDate){
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        return date.format(uneDate);
+    }
+}
